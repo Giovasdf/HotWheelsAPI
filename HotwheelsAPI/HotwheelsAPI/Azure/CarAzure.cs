afce6994cd8d9f75c33addc495f696fd403e6ed3
Giovanni Molina <gi.molina@profesor.duoc.cl>
2020-09-21 14:22:01 -0300

    First Commit
    HTTP Get all Cars

---
+++ HotwheelsAPI/HotwheelsAPI/Azure/CarAzure.cs
@@ -0,0 +1,45 @@
+﻿using HotwheelsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotwheelsAPI.Azure
+{
+    public class CarAzure
+    {
+        static readonly string ConnectionString = @"Server=DESKTOP-S6JN4IA\SQLEXPRESS;Database=Hotwheels;Trusted_Connection=True;";
+
+        public static List<Car> listCars;
+
+        public static List<Car> getAllCars()
+        {
+            var dt = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(null, connection);
+                command.CommandText = "select * from Car";
+
+                //abrir la conexion
+                connection.Open();
+                //ejecutar query
+                var dataAdapter = new SqlDataAdapter(command);
+                dataAdapter.Fill(dt);
+
+                listCars = new List<Car>();
+                Car car = new Car();
+                car.IdCar = Convert.ToInt32(dt.Rows[0]["idCar"].ToString());
+                car.Name = dt.Rows[0]["name"].ToString();
+                //...
+
+                listCars.Add(car);
+
+            }
+            return listCars;
+        }
+
+    }
+}
---
+++ HotwheelsAPI/HotwheelsAPI/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotwheelsAPI.Models
+{
+    public class Car
+    {
+        public int IdCar { get; set; }
+        public int Year { get; set; }
+        public string Serie { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Colour { get; set; }
+    }
+}
